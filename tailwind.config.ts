ed4c9cf5a6b4e9b545bb5e694d09c14b7537d107
Day <149915912+Daradeeday@users.noreply.github.com>
2025-08-02 15:16:20 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,9 @@ const config: Config = {
     },
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
